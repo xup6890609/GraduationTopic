23a4552c43b0e0be32b45ba49e80794965ef2da3
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2022-02-24 15:48:15 +0800

    跳躍bug修正

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -132,7 +132,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        PhysicsCheck();
+        //PhysicsCheck();
         GroundMovement();
         Jump();
         Run();
